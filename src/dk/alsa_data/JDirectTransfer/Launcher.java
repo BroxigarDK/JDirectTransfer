e25da72876dccf6cd9593f4e02e83ca8b61444f3
Anders Olsen <anders.d.olsen@gmail.com>
2011-09-11 00:38:14 +0200

    Trying out Git

---
+++ src/dk/alsa_data/JDirectTransfer/Launcher.java
@@ -0,0 +1,12 @@
+package dk.alsa_data.JDirectTransfer;
+
+public class Launcher {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+System.out.println("Learning how to use Git!");
+	}
+
+}
